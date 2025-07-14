05b31cf9b9a613eb90a644957369470f871aa360
Raj Baldha <rajbaldha5146@gmail.com>
2025-07-14 17:25:25 +0530

    six commit

--- frontend/src/environment.js
+++ frontend/src/environment.js
@@ -1,8 +1,8 @@
 let IS_PROD = true;
 const server = IS_PROD ?
-    // "https://zoom-app-nity.onrender.com" :
+    "https://zoom-app-nity.onrender.com" :
 
-    "http://localhost:8000":
+    // "http://localhost:8000":
     "http://localhost:8000"
 
 
